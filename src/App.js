a8e24928a967c184dbe03032aa82c731a7831694
bagcibaran <bagcibaran4@gmail.com>
2023-03-15 18:59:36 +0300

    Creating Components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import "./style/style.css";
 import data from "./data";
+import Card from "./components/Card";
+import NotFoundCard from "./components/NotFoundCard";
 
 function App() {
   const [filterData, setFilterData] = useState(data);
@@ -16,7 +18,7 @@ function App() {
 
   return (
     <div className="App">
-      <div className="container-fluid">
+      <div className="container">
         <div className="inputContainer">
           <button className="button" type="button">
             <i class="fa-sharp fa-solid fa-magnifying-glass"></i>
@@ -30,35 +32,19 @@ function App() {
           />
         </div>
 
-        <div className="list">
-          <div class="row">
-            {filterData.map((item, index) => (
-              <div className="card">
-                <div key={index} class="col-md-12">
-                  <div className="name">{item.name}</div>
-                  <div class="row">
-                    <div class="col-md-3">
-                      <a href="tel: {item.phone}">
-                        <i class="fa-solid fa-phone"></i>
-                        {item.phone}
-                      </a>
-                    </div>
-                    <div class="col-md-3">
-                      <a href="mailto: {item.email}">
-                        <i class="fa-solid fa-at"></i>
-                        {item.email}
-                      </a>
-                    </div>
-                    <div class="col-md-6">
-                      <i class="fa-solid fa-file-lines"></i>
-                      Policy No. {item.policyNo}
-                    </div>
-                  </div>
-                </div>
-              </div>
-            ))}
+        {search.length ? (
+          <div className="list">
+            <div class="list-item">
+              {filterData.length > 0 ? (
+                filterData.map((item, index) => (
+                  <Card item={item} index={index} />
+                ))
+              ) : (
+                <NotFoundCard />
+              )}
+            </div>
           </div>
-        </div>
+        ) : null}
       </div>
     </div>
   );
